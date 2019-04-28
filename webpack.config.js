cb510286d49ea0aeea4367960640a19d129414f7
Cameron Cummings <cameron.cummings@tecuity.com>
2019-04-28 13:34:18 -0600

    Initialize react boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// webpack needs to know the entry point and where to output
+// module.exports is from Node.js
+// https://webpack.js.org/
+
+const path = require('path');
+// console.log(path.join(__dirname, 'public'));
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+};
+
